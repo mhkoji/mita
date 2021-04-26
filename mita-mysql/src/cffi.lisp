8284c84fd3ca2988d6623a8bd1792e47c729c226
mhkoji <mhkoji2+git@gmail.com>
2021-04-26 22:34:21 +0900

    mita-mysql: Update

--- mita-mysql/src/cffi.lisp
+++ mita-mysql/src/cffi.lisp
@@ -23,6 +23,10 @@
 (cffi:defcfun ("mysql_errno" mysql-errno) :unsigned-int
   (mysql :pointer))
 
+(cffi:defcfun ("mysql_query" mysql-query) :int
+  (mysql :pointer)
+  (statement :string))
+
 (cffi:defcfun ("mysql_real_connect" mysql-real-connect) :pointer
   (mysql :pointer)
   (host :string)
@@ -32,3 +36,113 @@
   (port :int)
   (unix-socket :string)
   (client-flag :unsigned-long))
+
+(cffi:defcfun ("mysql_free_result" mysql-free-result) :void
+  (mysql-res :pointer))
+
+
+(cffi:defcfun ("mysql_num_fields" mysql-num-fields) :unsigned-int
+  (mysql-res :pointer))
+
+(cffi:defcfun ("mysql_fetch_fields" mysql-fetch-fields) :pointer
+  (mysql-res :pointer))
+
+(cffi:defcfun ("mysql_stmt_result_metadata" mysql-stmt-result-metadata)
+    :pointer
+  (mysql-stmt :pointer))
+
+(cffi:defcfun ("mysql_stmt_init" mysql-stmt-init) :pointer
+  (mysql :pointer))
+
+(cffi:defcfun ("mysql_stmt_prepare" mysql-stmt-prepare) :int
+  (stmt :pointer))
+
+(cffi:defcfun ("mysql_stmt_prepare" mysql-stmt-prepare) :int
+  (stmt :pointer)
+  (stmt-str :string)
+  (length :unsigned-long))
+
+(cffi:defcfun ("mysql_stmt_bind_param" mysql-stmt-bind-param) :boolean
+  (stmt :pointer)
+  (bind :pointer))
+
+(cffi:defcfun ("mysql_stmt_bind_result" mysql-stmt-bind-result) :boolean
+  (stmt :pointer)
+  (bind :pointer))
+
+(cffi:defcfun ("mysql_stmt_next_result" mysql-stmt-next-result) :int
+  (stmt :pointer))
+
+(cffi:defcfun ("mysql_stmt_execute" mysql-stmt-execute) :int
+  (stmt :pointer))
+
+(cffi:defcfun ("mysql_stmt_fetch" mysql-stmt-fetch) :int
+  (stmt :pointer))
+
+(cffi:defcfun ("mysql_stmt_close" mysql-stmt-close) :boolean
+  (stmt :pointer))
+
+(cffi:defcstruct mysql-field
+  (name :string)
+  (org-name :string)
+  (table :string)
+  (org-table :string)
+  (db :string)
+  (catalog :string)
+  (def :string)
+  (length :unsigned-long)
+  (max-length :unsigned-long)
+  (name-length :unsigned-int)
+  (org-name-length :unsigned-int)
+  (table-length :unsigned-int)
+  (org-table-length :unsigned-int)
+  (db-length :unsigned-int)
+  (catalog-length :unsigned-int)
+  (def-length :unsigned-int)
+  (flags :unsigned-int)
+  (decimals :unsigned-int)
+  (charsetnr :unsigned-int)
+  (type :int)
+  (extension :pointer))
+
+(cffi:defcstruct mysql-bind
+  (length :pointer)
+  (is-null :pointer)
+  (buffer :pointer)
+  (error :pointer)
+  (row-ptr :pointer)
+  (store-param-func :pointer)
+  (fetch-result-func :pointer)
+  (skip-result-func :pointer)
+  (buffer-length :unsigned-long)
+  (offset :unsigned-long)
+  (length-value :unsigned-long)
+  (param-number :unsigned-int)
+  (pack-length :unsigned-int)
+  (buffer-type :int)
+  (error-value :char)
+  (is-unsigned :char)
+  (long-data-used :char)
+  (is-null-value :char)
+  (extension :pointer))
+
+(cffi:defcenum enum-field-types
+  :decimal
+  :tiny
+  :short
+  :long
+  :float
+  :double
+  :null
+  :timestamp
+  :longlong
+  :int24
+  :date
+  :time
+  :datetime
+  :year
+  :newdate
+  :varchar
+  :bit
+  (:var-string 253)
+  (:string 254))
--- mita-mysql/src/mysql.lisp
+++ mita-mysql/src/mysql.lisp
@@ -21,3 +21,121 @@
 
 (defun disconnect (conn)
   (mita-mysql.cffi:mysql-close (connection-mysql conn)))
+
+(defun query (conn string)
+  (mita-mysql.cffi:mysql-query (connection-mysql conn) string))
+
+
+(defstruct param sql-type value)
+
+(defun convert-to-sql-value (sql-type value)
+  (ecase sql-type
+    ((:long :longlong)
+     value)
+    ((:string :var-string)
+     ;; TODO: free memory
+     (let ((buffer (cffi:foreign-alloc :char :count 1024)))
+       (cffi:lisp-string-to-foreign value buffer 1024)))))
+
+(defvar *mysql-bind-struct*
+  '(:struct mita-mysql.cffi::mysql-bind))
+
+(defvar *mysql-field-struct*
+  '(:struct mita-mysql.cffi::mysql-field))
+
+(defvar *ptr-mysql-bind-struct*
+  '(:pointer (:struct mita-mysql.cffi::mysql-bind)))
+
+(defvar *ptr-mysql-field-struct*
+  '(:pointer (:struct mita-mysql.cffi::mysql-field)))
+
+(defmacro field-type (field)
+  `(cffi:foreign-slot-value ,field *ptr-mysql-field-struct* 'type))
+
+(defmacro bind-buffer (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'buffer))
+
+(defmacro bind-buffer-type (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'buffer-type))
+
+(defmacro bind-buffer-length (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'buffer-length))
+
+(defmacro bind-length (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'length))
+
+(defmacro bind-is-null (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'is-null))
+
+(defmacro bind-error (bind)
+  `(cffi:foreign-slot-value ,bind *ptr-mysql-bind-struct* 'error))
+
+(defun set-param-to-bind (bind p)
+  (with-accessors ((value param-value)
+                   (sql-type param-sql-type)) p
+    (setf (bind-buffer bind)      (convert-to-sql-value sql-type value)
+          (bind-buffer-type bind) (cffi:foreign-enum-value
+                                   'enum-field-types sql-type)))
+  ;; TODO: free memory
+  (setf (bind-length bind)  (cffi:foreign-alloc :int)
+        (bind-is-null bind) (cffi:foreign-alloc :char)
+        (bind-error bind)   (cffi:foreign-alloc :char)))
+
+(defmacro with-stmt-result-metadata ((var stmt) &body body)
+  `(let ((,var (mita-mysql.cffi::mysql-stmt-result-metadata ,stmt)))
+     (unwind-protect (progn ,@body)
+       (mita-mysql.cffi::mysql-free-result ,var))))
+
+(defun fetch-result (stmt)
+  (with-stmt-result-metadata (res stmt)
+    ;; TODO: free memory
+    (let* ((num-fields (mita-mysql.cffi::mysql-num-fields res))
+           (binds (cffi:foreign-alloc
+                   *mysql-bind-struct* :count num-fields)))
+      (let ((fields (mita-mysql.cffi::mysql-fetch-fields res)))
+        (dotimes (i num-fields)
+          (let ((bind  (cffi:mem-aref binds *mysql-bind-struct* i))
+                (field (cffi:mem-aref fields *mysql-field-struct* i)))
+            ;; TODO: free memory
+            ;; TODO: should not restrict to  only int support
+            (setf (bind-buffer bind)      (cffi:foreign-alloc :int)
+                  (bind-buffer-type bind) (field-type field))
+            ;; TODO: free memory
+            (setf (bind-length bind)  (cffi:foreign-alloc :int)
+                  (bind-is-null bind) (cffi:foreign-alloc :char)
+                  (bind-error bind)   (cffi:foreign-alloc :char)))))
+      (loop do
+        (let ((status (mita-mysql.cffi::mysql-stmt-fetch stmt)))
+          (when (= status 1)
+            (return nil))
+          (dolist (i num-fields)
+            (print (cffi:mem-ref
+                    (bind-buffer
+                     (cffi:mem-aref binds *mysql-bind-struct* i))
+                    :int))))
+        (let ((status (mita-mysql.cffi::mysql-stmt-next-result stmt)))
+          (when (/= status 0)
+            (return)))))))
+
+(defun execute (conn query params)
+  (let ((mysql-stmt (mita-mysql.cffi::mysql-stmt-init
+                     (connection-mysql conn))))
+    (unwind-protect
+         (progn
+           ;; Prepare
+           (let ((len (length (babel:string-to-octets
+                               query :encoding :utf-8))))
+             (mita-mysql.cffi::mysql-stmt-prepare mysql-stmt query len))
+           ;; Bind
+           ;; TODO: free memory
+           (let ((binds (cffi:foreign-alloc
+                         *mysql-bind-struct* :count (length params))))
+             (loop for i from 0 for p in params do
+               (let ((bind (cffi:mem-aref binds *mysql-bind-struct* i)))
+                 (set-param-to-bind bind p)))
+             (mita-mysql.cffi::mysql-stmt-bind-param mysql-stmt binds))
+           ;; Execute
+           (mita-mysql.cffi::mysql-stmt-execute mysql-stmt)
+           ;; Fetch
+           (fetch-result mysql-stmt))
+      (mita-mysql.cffi::mysql-stmt-close mysql-stmt))))

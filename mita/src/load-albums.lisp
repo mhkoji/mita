5c91fe183cd0c76a9b8680d8a4e57d0bbeca5880
mhkoji <mhkoji2+git@gmail.com>
2021-09-13 23:31:54 +0900

    refactor

--- mita/src/load-albums.lisp
+++ mita/src/load-albums.lisp
@@ -17,15 +17,45 @@
                  (+ offset limit)
                  nil))))
 
-(defun run/query (db query offset limit &key on-loaded)
-  (multiple-value-bind (albums has-more)
-      (mita.db:with-connection (conn db)
-        (mita.album.list:run conn query offset limit))
-    (funcall on-loaded
-             albums
-             (if (< 0 offset)
-                 (max (- offset limit) 0)
-                 nil)
-             (if has-more
-                 (+ offset limit)
-                 nil))))
+(defun run/query (db q-string offset limit &key on-loaded)
+  (labels ((json->sort (json)
+             (mita.album.list:make-sort
+              (alexandria:make-keyword
+               (string-upcase (jsown:val json "field")))
+              (alexandria:make-keyword
+               (string-upcase (jsown:val json "direction")))))
+           (json->condition-item (json)
+             (list (alexandria:make-keyword
+                    (string-upcase (jsown:val json "op")))
+                   (alexandria:make-keyword
+                    (string-upcase (jsown:val json "field")))
+                   (jsown:val json "input")))
+           (json->query (json)
+             (let ((sort
+                    (when (jsown:keyp json "sort")
+                      (json->sort (jsown:val json "sort"))))
+                   (condition
+                    (when (jsown:keyp json "condition")
+                      (let ((items (mapcar #'json->condition-item
+                                           (jsown:val json "condition"))))
+                        (when items
+                          (labels ((rec (node rest)
+                                     (if (null rest)
+                                         node
+                                         (rec `(:and ,node ,(car rest))
+                                              (cdr rest)))))
+                            (rec (car items) (cdr items))))))))
+               (make-instance 'mita.album.list:query
+                              :condition condition :sort sort))))
+    (let ((query (json->query (jsown:parse q-string))))
+      (multiple-value-bind (albums has-more)
+          (mita.db:with-connection (conn db)
+            (mita.album.list:run conn query offset limit))
+        (funcall on-loaded
+                 albums
+                 (if (< 0 offset)
+                     (max (- offset limit) 0)
+                     nil)
+                 (if has-more
+                     (+ offset limit)
+                     nil))))))
--- mita/src/web/album.lisp
+++ mita/src/web/album.lisp
@@ -12,40 +12,9 @@
           (funcall on-not-found)))))
 
 (defun load-albums (dep req q offset limit &key on-loaded)
-  (labels ((json->sort (json)
-             (mita.album.list:make-sort
-              (alexandria:make-keyword
-               (string-upcase (jsown:val json "field")))
-              (alexandria:make-keyword
-               (string-upcase (jsown:val json "direction")))))
-           (json->condition-item (json)
-             (list (alexandria:make-keyword
-                    (string-upcase (jsown:val json "op")))
-                   (alexandria:make-keyword
-                    (string-upcase (jsown:val json "field")))
-                   (jsown:val json "input"))))
     (let ((db (mita.web.dep:get-db dep req)))
-      (if (not q)
-          (mita.load-albums:run db offset limit :on-loaded on-loaded)
-          (let* ((q-json
-                  (jsown:parse q))
-                 (sort
-                  (when (jsown:keyp q-json "sort")
-                    (json->sort (jsown:val q-json "sort"))))
-                 (condition
-                  (when (jsown:keyp q-json "condition")
-                    (let ((items (mapcar #'json->condition-item
-                                         (jsown:val q-json "condition"))))
-                      (when items
-                        (labels ((rec (node rest)
-                                   (if (null rest)
-                                       node
-                                       (rec `(:and ,node ,(car rest))
-                                            (cdr rest)))))
-                          (rec (car items) (cdr items)))))))
-                 (query
-                  (make-instance 'mita.album.list:query
-                                 :condition condition
-                                 :sort sort)))
-            (mita.load-albums:run/query
-             db query offset limit :on-loaded on-loaded))))))
+      (if q
+          (mita.load-albums:run/query db q offset limit
+           :on-loaded on-loaded)
+          (mita.load-albums:run db offset limit
+           :on-loaded on-loaded))))
